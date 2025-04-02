85ec4e258c6f0f493603f6c039ee0b87dfcafbd8
purpleoctopus <denysostapuk7@gmail.com>
2025-04-02 23:03:51 +0300

    run migration on webapi launch

--- CodeDinner.API/Program.cs
+++ CodeDinner.API/Program.cs
@@ -52,6 +52,12 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
