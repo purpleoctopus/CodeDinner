eb50f8588963793a41f0ec6b29ada0d79192318a
purpleoctopus <denysostapuk7@gmail.com>
2025-04-02 23:20:59 +0300

    UI create login form component

---
+++ wwwroot/CodeDinner/src/app/components/features/login-form/login-form.component.ts
@@ -0,0 +1,41 @@
+import {Component, Inject} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {CourseService} from '../../../services/course.service';
+
+@Component({
+  selector: 'app-login-form',
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.scss'
+})
+export class LoginFormComponent {
+
+  constructor(
+    private dialogRef: MatDialogRef<LoginFormComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { message: string },
+    private courseService: CourseService) { }
+
+  form = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  public async submit(){
+    if(this.form.valid){
+      const res = await this.courseService.createCourse({
+        username: this.form.value.username,
+        language: Number(this.form.value.language) ?? 0
+      });
+      this.dialogRef.close(res);
+    }
+  }
+}
