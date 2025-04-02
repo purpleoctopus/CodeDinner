df920e420c3984dfd21583cae0a0bc71edf7e4b4
purpleoctopus <denysostapuk7@gmail.com>
2025-04-02 23:20:18 +0300

    auth controller refactoring

--- CodeDinner.API/Controllers/AuthController.cs
+++ CodeDinner.API/Controllers/AuthController.cs
@@ -3,11 +3,18 @@ using Microsoft.AspNetCore.Mvc;
 namespace CodeDinner.API.Controllers;
 
 [Route("api/[controller]/[action]")]
-public class UserController : ControllerBase
+public class AuthController : ControllerBase
 {
-    public UserController()
+    public AuthController()
     {
         
+    }
+    
+    [HttpPost]
+    [Route("[action]")]
+    public async Task<IActionResult> Login()
+    {
+        return Ok();
     }
     
     [HttpPost]
