811fc5a7a89367a8d5409d3826509b3fcbc0671a
purpleoctopus <denysostapuk7@gmail.com>
2025-05-04 15:51:13 +0300

    BE: clean commented code

--- CodeBreakfast.API/Program.cs
+++ CodeBreakfast.API/Program.cs
@@ -74,12 +74,6 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-/*using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    dbContext.Database.Migrate();
-}*/
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
