d2ba0d1360297533f9765408d601d35b5f38a647
purpleoctopus <denysostapuk7@gmail.com>
2025-05-06 15:34:13 +0300

    BE: provide username in session model

--- CodeBreakfast.Common/Models/SessionModel.cs
+++ CodeBreakfast.Common/Models/SessionModel.cs
@@ -2,6 +2,7 @@ namespace CodeBreakfast.Common.Models;
 
 public class SessionModel
 {
+    public string Username { get; set; }
     public string AccessToken { get; set; }
     public List<string> Roles { get; set; }
 }
